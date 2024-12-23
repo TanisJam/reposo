a9f4ec6ea73f1948502b8148d053d5dcef087475
Mauricio Romero <mauricionromero@hotmail.com>
2024-12-23 20:56:51 -0300

    fix font

--- app/_lib/links.ts
+++ app/_lib/links.ts
@@ -2,6 +2,6 @@ export const LINKS = {
   discordInvite: 'https://discord.com/invite/pqQ4DSxQp7',
   forgottenWiki: 'https://forgottenrealms.fandom.com',
   tools: 'https://2014.5e.tools',
-  atlas: 'https://www.aidedd.org/public/atlas/index.php?map=R&l=2',
+  atlas: 'https://www.aidedd.org/public/atlas/index.php?map=R&l=1',
   reference: 'https://homebrewery.naturalcrit.com/share/RZ74iEuYltmy',
 };
